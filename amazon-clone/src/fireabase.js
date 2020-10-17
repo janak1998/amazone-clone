e59721954a3af162784ccde9665a6854f4878954
janak bohra <janakbohra29@gmail.com>
2020-10-17 18:05:52 +0530

    made react app and added firebaseconfig

---
+++ amazon-clone/src/fireabase.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBLjEZP3OxB0hVIB8eAN3jBaZZR1JDy8Ik",
+    authDomain: "clone-e3b80.firebaseapp.com",
+    databaseURL: "https://clone-e3b80.firebaseio.com",
+    projectId: "clone-e3b80",
+    storageBucket: "clone-e3b80.appspot.com",
+    messagingSenderId: "662516073989",
+    appId: "1:662516073989:web:c0602b2c73ee7345503e6c",
+    measurementId: "G-6ZDVERGMXW"
+  };
